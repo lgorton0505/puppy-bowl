3e9267d51f55b2cb52487910679529fe20a4b870
Lance Gorton <lance@Lances-MacBook-Air.local>
2023-12-19 13:08:41 -0600

    ugh fixed it

--- app.js
+++ app.js
@@ -1,37 +1,36 @@
-const span = document.querySelector('span');
-const ul = document.querySelector('ul');
+const span = document.querySelector("span");
+const ul = document.querySelector("ul");
 let puppies = [];
-console.log(puppies);
 
 function render(){
-   span.innerHTML = puppies.length;
-   const hash = window.location.hash;
-   const id = hash.slice(1)*1;
-   let filtered = puppies;
-   console.log(filtered);
-   if(id){
-      filtered = filtered.filter(function(puppy){
-         return puppy.id === id; 
-      });
-   }
-    const html = filtered.map(function(puppy){
-        return `
-           <li>
-              <h4><a href='#${puppy.id}'>${ puppy.name }</a><h4>
-           </li>
-        `;
-    }).join('');
-    ul.innerHTML = html;
+  span.innerHTML = puppies.length;
+  const hash = window.location.hash;
+  const id = hash.slice(1)*1;
+  let filtered = puppies;
+  console.log(filtered);
+  if(id){
+     filtered = filtered.filter(function(puppy){
+        return puppy.id === id; 
+     });
+  }
+   const html = filtered.map(function(puppy){
+       return `
+          <li>
+             <h4><a href='#${puppy.id}'>${ puppy.name }</a><h4>
+          </li>
+       `;
+   }).join('');
+   ul.innerHTML = html;
 }
 
 render();
 
-async function fetchEvents(){
-    const response = await fetch('https://fsa-puppy-bowl.herokuapp.com/api/2309-ftb-et-am/players');
-    const json = await response.json();
-    console.log(json);
-    puppies = json.data;
-    render();
+async function getPuppies() {
+  const res = await fetch("https://fsa-puppy-bowl.herokuapp.com/api/2309-ftb-et-am/players");
+  const json = await res.json();
+  puppies = json.data.players;
+  console.log(puppies);
+  render();
 }
 
-fetchEvents();
+getPuppies();
