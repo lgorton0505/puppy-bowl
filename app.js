ee513278097b34cefb310d2fa7182e685045c822
Lance Gorton <lance@Lances-MacBook-Air.local>
2023-12-21 08:46:53 -0600

    Finished

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ const numSpan = document.querySelector("#puppyNumber");
 const goBackSpan = document.querySelector("#goBack");
 const ul = document.querySelector("ul");
 let puppies = [];
+let singleItem = false;
 
 window.addEventListener('hashchange', render);
 
 
+/**
+ * Renders the puppies on the page based on the current filter.
+ */
 function render(){
   numSpan.innerHTML = puppies.length;
   const hash = window.location.hash;
@@ -17,20 +21,35 @@ function render(){
         return puppy.id === id; 
      });
   }
+
+  /// If there is only one puppy, show the "Back to Puppies" link
   if (filtered.length === 1) {
-    goBackSpan.innerHTML = "Go back";
+    goBackSpan.innerHTML = "Back to Puppies";
     goBackSpan.style.display = "inline";
+    singleItem = true;
   } else {
     goBackSpan.style.display = "none";
+    singleItem = false;
   }
+
    const html = filtered.map(function(puppy){
-       return `
-          <li>
-             <h4><a href='#${puppy.id}'>${ puppy.name }</a><h4>
-             <p>${ puppy.breed }</p>
-             <img src='${ puppy.imageUrl }' />
+      if (singleItem === true) {
+        return `
+          <li id='singleItem'>
+             <h4><a href='#${puppy.id}'>${puppy.name}</a><h4>
+             <p>${puppy.breed}</p>
+             <img src='${puppy.imageUrl}' />
           </li>
        `;
+      } else {
+        return `
+          <li>
+             <h4><a href='#${puppy.id}'>${puppy.name}</a><h4>
+             <p>${puppy.breed}</p>
+             <img src='${puppy.imageUrl}' />
+         </li>
+      `;
+      }
    }).join('');
    ul.innerHTML = html;
 }
